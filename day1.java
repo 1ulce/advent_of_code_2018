30ea7cd647d017e391dec1526f7162877dcfb099
Shun Tsukuda <shun.tsukuda@gmail.com>
2018-12-06 13:54:01 +0900

    day1-1

---
+++ day1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        String filename = "/Users/shun/Code/small-tasks/adventofcode2018/day1.txt";
+        try (BufferedReader in = new BufferedReader(new FileReader(new File(filename)))){
+            String line;
+            Integer answer = 0;
+            while((line = in.readLine()) != null) {
+              //System.out.println(line);
+              answer += Integer.parseInt(line);
+            }
+            System.out.println(answer);
+        } catch (FileNotFoundException e){ 
+            e.printStackTrace();
+            System.exit(-1); // 0 以外は異常終了
+        } catch (IOException e){ 
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }   
+}
